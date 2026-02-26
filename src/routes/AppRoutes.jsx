64dafd22b8fd083c7de2b9040e36f8b74f8c7ced
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-02-25 21:41:37 -0700

    Setup content for route testing

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from "react-router";
-import App from "./App.jsx";
+import App from "../App.jsx";
 
-import Home from "./pages/Home/Home.jsx";
-import Posts from "./pages/Posts/Posts.jsx";
-import PostDetail from "./pages/PostDetail/PostDetail.jsx";
-import Register from "./pages/Register/Register.jsx";
-import Login from "./pages/Login/Login.jsx";
-import Dashboard from "./pages/Dashboard/Dashboard.jsx";
-import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+import Home from "../pages/Home/Home.jsx";
+import Posts from "../pages/Posts/Posts.jsx";
+import PostDetail from "../pages/PostDetail/PostDetail.jsx";
+import Register from "../pages/Register/Register.jsx";
+import Login from "../pages/Login/Login.jsx";
+import Dashboard from "../pages/Dashboard/Dashboard.jsx";
+import ErrorPage from "../pages/ErrorPage/ErrorPage.jsx";
 
 const router = createBrowserRouter([
 	{
