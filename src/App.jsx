a3de2afb5bfc5c0961a4719ec03b389ef4692f6d
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-02-25 20:00:17 -0700

    Setup files and layout routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState, useEffect } from "react";
+import { Outlet } from "react-router";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
 	return (
 		<>
-			<div></div>
+			<Header />
+			<main>
+				<Outlet />
+			</main>
+			<Footer />
 		</>
 	);
 }
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router";
+import App from "./App.jsx";
+
+import Home from "./pages/Home/Home.jsx";
+import Posts from "./pages/Posts/Posts.jsx";
+import PostDetail from "./pages/PostDetail/PostDetail.jsx";
+import Register from "./pages/Register/Register.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Dashboard from "./pages/Dashboard/Dashboard.jsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorPage />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: "posts", element: <Posts /> },
+			{ path: "posts/:slug", element: <PostDetail /> },
+			{ path: "login", element: <Login /> },
+			{ path: "register", element: <Register /> },
+			{ path: "dashboard", element: <Dashboard /> },
+		],
+	},
+]);
+
+export default router;
