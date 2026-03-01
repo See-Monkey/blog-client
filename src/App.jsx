274225714f7761859e84b2a919f7729b7b2c614b
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-03-01 16:16:03 -0700

    Add color theme & fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import "./app.css";
 
 function App() {
 	return (
