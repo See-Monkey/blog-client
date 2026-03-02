5d82fbf56ac8d732bf534abd75dc194b3a327295
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-03-01 19:48:13 -0700

    Create & style header & footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,23 @@
+import styles from "./Footer.module.css";
+import githubLogo from "../../icons/github.svg";
+
 export default function Footer() {
 	return (
-		<footer>
-			<h2>Footer Component</h2>
+		<footer className={styles.footer}>
+			<div className={styles.footerContainer}>
+				<h4 className={styles.createdBy}>Created by See-Monkey</h4>
+				<div className={styles.githubContainer}>
+					<img src={githubLogo} alt="Github" className={styles.githubLogo} />
+					<div className={styles.linkContainer}>
+						<a href="https://github.com/See-Monkey/blog-api">
+							Backend Express API
+						</a>
+						<a href="https://github.com/See-Monkey/blog-client">
+							Frontend React App
+						</a>
+					</div>
+				</div>
+			</div>
 		</footer>
 	);
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,88 @@
 import { useAuth } from "../../context/useAuth.js";
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
+import styles from "./Header.module.css";
+import logo from "../../images/see-monkey-logo-alpha-black.png";
 
 export default function Header() {
 	const { isAdmin, isAuthenticated, logout } = useAuth();
 
 	return (
-		<header>
-			<nav>
-				<Link to="/">Home</Link>
-				<Link to="/posts">Posts</Link>
-
-				{isAdmin && <Link to="/dashboard">Admin Dashboard</Link>}
-
-				{isAuthenticated ? (
-					<button onClick={logout}>Logout</button>
-				) : (
-					<>
-						<Link to="/login">Login</Link>
-						<Link to="/register">Register</Link>
-					</>
-				)}
-			</nav>
+		<header className={styles.header}>
+			<div className={styles.headerContainer}>
+				<div className={styles.headerTop}>
+					<div className={styles.logoContainer}>
+						<div className={styles.imgContainer}>
+							<img
+								src={logo}
+								alt="monkey with glasses"
+								className={styles.logoImg}
+							/>
+						</div>
+						<div className={styles.logoTextContainer}>
+							<h2 className={styles.logoFront}>Monkey</h2>
+							<h1 className={styles.logoCenter}>See-Monkey</h1>
+							<h2 className={styles.logoEnd}>Do</h2>
+						</div>
+					</div>
+					<div className={styles.authContainer}>
+						{isAuthenticated ? (
+							<button onClick={logout} className={styles.logoutBtn}>
+								Logout
+							</button>
+						) : (
+							<>
+								<NavLink
+									to="/login"
+									className={({ isActive }) =>
+										isActive ? styles.active : undefined
+									}
+								>
+									Login
+								</NavLink>
+								<NavLink
+									to="/register"
+									className={({ isActive }) =>
+										isActive ? styles.active : undefined
+									}
+								>
+									Register
+								</NavLink>
+							</>
+						)}
+					</div>
+				</div>
+				<div className={styles.headerBottom}>
+					<nav className={styles.nav}>
+						<NavLink
+							to="/"
+							end
+							className={({ isActive }) =>
+								isActive ? styles.active : undefined
+							}
+						>
+							Home
+						</NavLink>
+						<NavLink
+							to="/posts"
+							className={({ isActive }) =>
+								isActive ? styles.active : undefined
+							}
+						>
+							Posts
+						</NavLink>
+						{isAdmin && (
+							<NavLink
+								to="/dashboard"
+								className={({ isActive }) =>
+									isActive ? styles.active : undefined
+								}
+							>
+								Dashboard
+							</NavLink>
+						)}
+					</nav>
+				</div>
+			</div>
 		</header>
 	);
 }
