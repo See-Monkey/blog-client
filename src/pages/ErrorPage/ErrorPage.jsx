ea47a19fa433f2660101fd6bad13f6f3e7a605fe
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-03-06 16:37:58 -0700

    Add confused monkey error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,3 +1,22 @@
+import styles from "./ErrorPage.module.css";
+import confusedMonkey from "../../images/confused-monkey.png";
+import { Link } from "react-router";
+
 export default function ErrorPage() {
-	return <h1>Error Page</h1>;
+	return (
+		<main className={styles.main}>
+			<div className={styles.errorContainer}>
+				<h1>Hmm, something isn't right</h1>
+				<div className={styles.errorContent}>
+					<img src={confusedMonkey} alt="confused monkey" />
+					<p>
+						The page you're looking for doesn't seem to exist or has been moved.
+					</p>
+				</div>
+				<p>
+					Click <Link to={"/"}>here</Link> to go back to safety.
+				</p>
+			</div>
+		</main>
+	);
 }
