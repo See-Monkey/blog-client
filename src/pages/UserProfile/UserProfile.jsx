89eb75890b2feab03a0950e4e3d67a362af7ab27
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-03-07 09:30:14 -0700

    Add paginated comments to UserProfile

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -48,6 +48,8 @@ export default function UserProfile() {
 
 				const data = await getCommentsByUser({ userId, page, limit: 10 });
 
+				console.log("API comments:", data);
+
 				setComments(data.comments);
 				setTotalPages(data.totalPages);
 				setCurrentPage(data.currentPage);
@@ -59,7 +61,6 @@ export default function UserProfile() {
 			}
 		};
 		fetchComments();
-		setCurrentPage(1);
 	}, [userId, page]);
 
 	if (userLoading) return <p>Loading...</p>;
@@ -95,6 +96,69 @@ export default function UserProfile() {
 
 			<div className={styles.commentsContainer}>
 				<h2>Comments</h2>
+
+				{isAuthenticated ? (
+					<>
+						{commentLoading ? (
+							<p>Loading...</p>
+						) : (
+							<>
+								{comments.map((comment) => {
+									const createdDate = formatDateTime(comment.createdAt);
+									const editedDate = formatDateTime(comment.updatedAt);
+
+									console.log(comments);
+
+									return (
+										<div key={comment.id} className={styles.commentContainer}>
+											<div className={styles.commentContentContainer}>
+												<h4>Post:</h4>
+												<Link
+													to={`/posts/${comment.post.slug}`}
+													className={styles.postLink}
+												>
+													<p>{comment.post.title}</p>
+												</Link>
+
+												<p>{comment.content}</p>
+
+												<p className={styles.createdDate}>{createdDate}</p>
+
+												{comment.updatedAt !== comment.createdAt && (
+													<p className={styles.editedDate}>
+														Edited: {editedDate}
+													</p>
+												)}
+											</div>
+										</div>
+									);
+								})}
+
+								{totalPages > 1 && (
+									<div className={styles.pageContainer}>
+										{hasPrevious && (
+											<Link to={`/users/${userId}?page=${currentPage - 1}`}>
+												<button className={styles.previousBtn}>Previous</button>
+											</Link>
+										)}
+
+										<p>
+											Page {currentPage} of {totalPages}
+										</p>
+
+										{hasNext && (
+											<Link to={`/users/${userId}?page=${currentPage + 1}`}>
+												<button className={styles.nextBtn}>Next</button>
+											</Link>
+										)}
+									</div>
+								)}
+							</>
+						)}
+					</>
+				) : (
+					<p>Login to see this user's comments</p>
+				)}
 			</div>
 
 			{/* Admin delete user */}
